8c933fd63eacef54f2996dcd8d336feb8a6542e8
zux13 <zux.grr@gmail.com>
2023-02-27 10:43:47 +0500

    Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,23 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+    public String surname;
+
+    private int sizeOfEmpty = 10;
+    String name;
+
+    void echo() { }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+
+    Broken() { }
+}
