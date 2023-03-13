3fb187862d0b8493743528182bed650f3855c71b
zux13 <zux.grr@gmail.com>
2023-03-13 09:15:52 +0500

    8. Date. Отображение даты. [#310017]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,16 @@
+package ru.job4j.tracker;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class StartUI {
+
+    public static void main(String[] args) {
+        Item item = new Item();
+        LocalDateTime created = item.getCreated();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        String createdFormat = created.format(formatter);
+        System.out.println(createdFormat);
+    }
+
+}
