813d1caae61148aa4918e4e24ce8cedafd00004c
zux13 <zux.grr@gmail.com>
2023-03-14 11:28:08 +0500

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,32 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        int counter = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                rsl[counter] = items[i];
+                counter++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, counter);
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[size];
+        int counter = 0;
+        for (int i = 0; i < size; i++) {
+            if (key.equals(items[i].getName())) {
+                rsl[counter] = items[i];
+                counter++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, counter);
+        return rsl;
     }
 
     public Item findById(int id) {
