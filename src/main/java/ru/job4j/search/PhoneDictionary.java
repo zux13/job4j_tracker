f993206e24eb6ed165f1c1cac95829ff0928e693
zux13 <zux.grr@gmail.com>
2023-10-02 08:18:41 +0500

    4. Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,29 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    /**
+     * Вернуть список всех пользователей, который содержат key в любых полях.
+     * @param key Ключ поиска.
+     * @return Список пользователей, которые прошли проверку.
+     */
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person value : persons) {
+            if (value.getName().contains(key)
+                    || value.getSurname().contains(key)
+                    || value.getPhone().contains(key)
+                    || value.getAddress().contains(key)) {
+                result.add(value);
+            }
+        }
+        return result;
+    }
+}
