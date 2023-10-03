a09e5c10441c1108a42b914688cb1178971fbdc2
zux13 <zux.grr@gmail.com>
2023-10-03 07:29:12 +0500

    5. Очередь с приоритетом на LinkedList [#41670 #449441]

--- src/main/java/ru/job4j/search/PriorityQueue.java
+++ src/main/java/ru/job4j/search/PriorityQueue.java
@@ -14,10 +14,10 @@ public class PriorityQueue {
     public void put(Task task) {
         int index = 0;
         for (Task element : tasks) {
-            if (task.getPriority() >= element.getPriority()) {
-                index++;
+            if (element.getPriority() > task.getPriority()) {
                 break;
             }
+            index++;
         }
         this.tasks.add(index, task);
     }
