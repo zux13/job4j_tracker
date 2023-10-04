e663d397393ca52051102ad25bf3d0bbebf92163
zux13 <zux.grr@gmail.com>
2023-10-04 08:38:07 +0500

    2. Идентичные тексты [#212655]

---
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -0,0 +1,21 @@
+package ru.job4j.collection;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+public class UniqueText {
+    public boolean isEquals(String originText, String duplicateText) {
+        boolean rsl = true;
+        String[] origin = originText.split(" ");
+        String[] text = duplicateText.split(" ");
+        HashSet<String> check = new HashSet<>();
+        Collections.addAll(check, origin);
+        for (String word : text) {
+            if (!check.contains(word)) {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+}
