020bd912238ae76b51bf59d7a66ddb94a5c0f5de
zux13 <zux.grr@gmail.com>
2023-10-05 08:01:28 +0500

    1. Интерфейс Queue [#504942]

---
+++ src/main/java/ru/job4j/queue/AppleStore.java
@@ -0,0 +1,29 @@
+package ru.job4j.queue;
+
+import java.util.Queue;
+
+public class AppleStore {
+    private final Queue<Customer> queue;
+
+    private final int count;
+
+    public AppleStore(Queue<Customer> queue, int count) {
+        this.queue = queue;
+        this.count = count;
+    }
+
+    public String getLastHappyCustomer() {
+        String rsl = "";
+        for (int i = 0; i < count; i++) {
+            rsl = queue.poll().name();
+        }
+        return rsl;
+    }
+
+    public String getFirstUpsetCustomer() {
+        for (int i = 0; i < count; i++) {
+            queue.poll();
+        }
+        return queue.poll().name();
+    }
+}
