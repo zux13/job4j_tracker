59351ce3ea06d57d781857f34aa6b2448c078b50
zux13 <zux.grr@gmail.com>
2023-10-07 08:34:19 +0500

    0. Map, HashMap [#212652 #450547]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -5,7 +5,10 @@ import java.util.HashMap;
 public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
+        map.put("zux-rr@ya.ru", "Ilnur Safin");
         map.put("zux-rr@ya.ru", "Rustem Galiullin");
+        map.put("parsentev@yandex.ru", "Petr Arsentev");
+        map.put("igorjr@yandex.ru", "Igor Krimsky");
         for (String key: map.keySet()) {
             System.out.println(key + " = " + map.get(key));
         }
