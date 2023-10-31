ba24f01c2d981cff9157da0d8ecfd2708e7bca61
zux13 <zux.grr@gmail.com>
2023-10-31 09:15:58 +0500

    2. Что такое JavaDoc [#313879]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -5,17 +5,49 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Описывает банковский сервис.
+ * <p>
+ *
+ *
+ * @author Rustem Galiullin
+ * @version 1.0
+ */
 public class BankService {
+
+    /**
+     * Хранит список аккаунтов пользователей в коллекции типа HashMap, где ключ - {@link User},
+     * значение - List с типами {@link Account}.
+     */
     private final Map<User, List<Account>> users = new HashMap<>();
 
+    /**
+     * Добавляет новый элемент в коллекцию {@link #users} в случае когда в ней отсутствует переданный ключ.
+     *
+     * @param user пользователь типа {@link User}
+     */
     public void addUser(User user) {
         users.putIfAbsent(user, new ArrayList<Account>());
     }
 
+    /**
+     * Удаляет из коллекции {@link #users} элемент по паспорту.
+     *
+     * @param passport паспорт пользователя в строковом виде
+     */
     public void deleteUser(String passport) {
         users.remove(new User(passport, ""));
     }
 
+    /**
+     * Добавляет аккаунт пользователя в коллекцию, в случае когда его там нет.
+     * <p>
+     * Осуществляет поиск пользователя по паспорту, вызывая метод {@link #findByPassport(String)}. Если пользователь
+     * найден, получается список его аккаунтов и, в случае отсутствия в списке, переданный аккаунт добавляется.
+     *
+     * @param passport паспорт пользователя в строковом виде
+     * @param account  аккаунт пользователя {@link Account}
+     */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -26,6 +58,13 @@ public class BankService {
         }
     }
 
+    /**
+     * Осуществляет поиск пользователя {@link User} в коллекции {@link #users} по паспорту.
+     *
+     * @param passport паспорт пользователя в строковом виде
+     * @return <code>User</code> - если пользователь найден в коллекции.
+     *         <code>null</code> - если пользователь не найден в коллекции.
+     */
     public User findByPassport(String passport) {
         for (User usr : users.keySet()) {
             if (usr.getPassport().equals(passport)) {
@@ -35,6 +74,17 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Осуществляет поиск аккаунта {@link Account} в коллекции {@link #users} по паспорту и реквизитам.
+     * <p>
+     * Вызывается метод {@link #findByPassport(String)} и, в случае если пользователь найден, получает список
+     * его аккаунтов, среди которых ищется тот, чьи реквизиты совпадают с переданными в метод.
+     *
+     * @param passport  паспорт пользователя в строковом виде
+     * @param requisite реквизиты аккаунта пользователя в строковом виде
+     * @return          <code>Account</code> - если найден пользователь и у него есть аккаунт с переданными реквизитами.
+     *                  <code>null</code> - если не найден пользователь или не найден аккаунт.
+     */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
         if (user != null) {
@@ -48,6 +98,23 @@ public class BankService {
         return null;
     }
 
+    /**
+     * Осуществляет перевод средств от одного пользователя другому.
+     * <p>
+     * Для отправителя и получателя вызывает метод {@link #findByRequisite(String, String)}. В случае, если
+     * оба аккаунта найдены и на балансе аккаунта отправителя достаточно средств для перевода, осуществляет перевод
+     * на сумму <b>amount</b> и возвращает <code>true</code>.
+     *
+     * @param srcPassport   паспорт пользователя осуществляющего перевод
+     * @param srcRequisite  реквизиты аккаунта пользователя осуществляющего перевод
+     * @param destPassport  паспорт пользователя получающего перевод
+     * @param destRequisite реквизиты аккаунта пользователя получающего перевод
+     * @param amount        Сумма средств для перевода
+     * @return              <code>true</code> - если перевод успешен
+     *                      <code>false</code> - если на аккаунте пользователя недостаточно средств, если один или оба
+     *                      пользователя не найдены по паспорту, если один или оба аккаунта пользователей не найдены по
+     *                      реквизитам
+     */
     public boolean transferMoney(String srcPassport, String srcRequisite,
                                  String destPassport, String destRequisite, double amount) {
         boolean rsl = false;
@@ -62,6 +129,13 @@ public class BankService {
         return rsl;
     }
 
+    /**
+     * Получает список аккаунтов {@link Account} пользователя {@link User}, хранящихся в коллекции {@link #users}.
+     *
+     * @param user  пользователь <code>User</code>
+     * @return      <code>List<Account></code> - если переданный пользователь найден в коллекции
+     *              <code>null</code> - если переданный пользователь отсутствует в колекции
+     */
     public List<Account> getAccounts(User user) {
         return users.get(user);
     }
--- src/main/java/ru/job4j/bank/User.java
+++ src/main/java/ru/job4j/bank/User.java
@@ -2,31 +2,79 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Описывает пользователя сервиса {@link BankService} со свойствами <b>passport</b> и <b>username</b>.
+ *
+ * @author Rustem Galiullin
+ * @version 1.0
+ */
 public class User {
+
+    /**
+     * Хранит паспорт пользователя в строковом виде.
+     */
     private String passport;
+
+    /**
+     * Хранит имя пользователя в строковом виде.
+     */
     private String username;
 
+    /**
+     * Канонический конструктор класса.
+     *
+     * @param passport паспорт пользователя в строковом виде
+     * @param username имя пользователя в строковом виде
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Возвращает поле {@link #passport}.
+     *
+     * @return <code>String</code> - паспорт пользователя в виде строки
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Устанавливает значение поля {@link #passport}.
+     *
+     * @param passport паспорт пользователя в виде строки
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Возвращает поле {@link #username}.
+     *
+     * @return <code>String</code> - имя пользователя в виде строки
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Устанавливает значение поля {@link #username}.
+     *
+     * @param username имя пользователя в виде строки
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Сравнивает переданный объект с текущим.
+     *
+     * @param o Объект типа {@code Object} с которым проводится сравнение
+     * @return  <code>true</code> если в параметре передан текущий объект
+     *          или значение полей {@link #passport} сравниваемых объектов равны.
+     *          <code>false</code> во всех остальных случаях
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +87,12 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Возвращает значение хэша поля {@link #passport}.
+     *
+     * @return <code>int</code> - результат вызова метода {@link Objects#hash(Object...)}
+     *          с параметром {@link #passport}
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
