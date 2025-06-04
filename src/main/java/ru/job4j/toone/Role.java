fa03720490a7dac8c9a45b82eff4f3bd9e246712
Rustem Galiullin <zux.grr@gmail.com>
2025-06-04 15:05:53 +0500

    0. ToOne [#6873]

---
+++ src/main/java/ru/job4j/toone/Role.java
@@ -0,0 +1,21 @@
+package ru.job4j.toone;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "j_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Include
+    private int id;
+
+    private String name;
+
+}
---
+++ src/main/java/ru/job4j/toone/User.java
@@ -0,0 +1,25 @@
+package ru.job4j.toone;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.EqualsAndHashCode.Include;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "j_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Include
+    private int id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+}
